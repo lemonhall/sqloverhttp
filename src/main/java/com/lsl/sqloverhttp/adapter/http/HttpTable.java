8ed87c8899dfbe70340accf0a9a1ec7d22d43635
lemonhall <lemonhall2012@qq.com>
2020-07-15 10:39:03 +0800

    I can get url args,and get json from server now

--- src/main/java/com/lsl/sqloverhttp/adapter/http/HttpTable.java
+++ src/main/java/com/lsl/sqloverhttp/adapter/http/HttpTable.java
@@ -2,34 +2,36 @@ package com.lsl.sqloverhttp.adapter.http;
 
 import org.apache.calcite.DataContext;
 import org.apache.calcite.adapter.java.JavaTypeFactory;
-import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.linq4j.AbstractEnumerable;
 import org.apache.calcite.linq4j.Enumerable;
 import org.apache.calcite.linq4j.Enumerator;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.ScannableTable;
-import org.apache.calcite.schema.Schema;
-import org.apache.calcite.schema.Statistic;
-import org.apache.calcite.schema.Statistics;
 import org.apache.calcite.schema.impl.AbstractTable;
-import org.apache.calcite.sql.SqlCall;
-import org.apache.calcite.sql.SqlNode;
-
-import com.google.common.collect.ImmutableList;
 import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.util.Pair;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public class HttpTable extends AbstractTable implements ScannableTable {
 
+    //具体执行扫表的例程
     @Override
     public Enumerable<Object[]> scan(DataContext dataContext) {
+        try {
+            getHttp();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new AbstractEnumerable<Object[]>() {
             @Override
             public Enumerator<Object[]> enumerator() {
@@ -37,6 +39,8 @@ public class HttpTable extends AbstractTable implements ScannableTable {
             }
         };
     }
+
+    //搞定表里每列名字的例程
     @Override
     public RelDataType getRowType(RelDataTypeFactory relDataTypeFactory) {
         JavaTypeFactory typeFactory = (JavaTypeFactory)relDataTypeFactory;
@@ -49,5 +53,27 @@ public class HttpTable extends AbstractTable implements ScannableTable {
         types.add(typeFactory.createSqlType(SqlTypeName.VARCHAR));
 
         return typeFactory.createStructType(Pair.zip(names,types));
+    }
+
+    private String getHttp() throws IOException {
+        CloseableHttpResponse Response = null;
+        CloseableHttpClient client = HttpClients.createDefault();    //创建一个http客户端
+        HttpGet httpGet = new HttpGet("http://172.18.161.113:8888/"); // 通过httpget方式来实现我们的get请求
+        try {
+            //请求执行
+            Response = client.execute(httpGet); // 通过client调用execute方法，得到我们的执行结果就是一个response，所有的数据都封装在response里面了
+            if (Response.getStatusLine().getStatusCode() == 200) {
+                String content = EntityUtils.toString(Response.getEntity(), "utf-8");
+                System.out.println("Http respon--------->" + content);
+                return content;
+            }
+            return "";
+        } finally {
+            if (Response != null) {
+                Response.close();
+            }
+            Response.close();
+            return "";
+        }
     }
 }
--- src/main/java/com/lsl/sqloverhttp/adapter/http/HttpTableFactory.java
+++ src/main/java/com/lsl/sqloverhttp/adapter/http/HttpTableFactory.java
@@ -9,7 +9,13 @@ import java.util.Map;
 
 public class HttpTableFactory implements TableFactory {
     @Override
-    public Table create(SchemaPlus schemaPlus, String name, Map map, RelDataType relDataType) {
+    public Table create(SchemaPlus schemaPlus, String tableName, Map operand, RelDataType relDataType) {
+        //从上层解算出自己的Schema的参数
+        final HttpSchema httpSchema = schemaPlus.unwrap(HttpSchema.class);
+        //抄redis那边
+        String url = operand.get("url") == null ? null
+                : operand.get("url").toString();
+        System.out.println("tableName ==>"+tableName+"\nurl => "+url);
         return new HttpTable();
     }
 }
